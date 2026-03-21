7f85b03507ac70b21522d0a0506c12ac3f4aae3f
Abhinav Singh <abhinavsingh.yeah@gmail.com>
2026-03-21 11:30:03 +0530

    haha hehe

--- frontend/src/pages/Deploy.jsx
+++ frontend/src/pages/Deploy.jsx
@@ -90,7 +90,10 @@ export default function Deploy() {
       }
 
     setOutput(accRef.current);
-    if (accRef.current.includes("No Dockerfile found")) setShowDockerfileHelp(true);
+if (accRef.current.includes("No Dockerfile found")) {
+  alert("TRIGGER");
+  setShowDockerfileHelp(true);
+}
     } catch (err) {
       const msg = err?.message || "Deployment failed";
       setError(msg);
